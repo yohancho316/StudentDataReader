40ad455cb76418691b0bcd472281c46d6e8cef25
John Cho <yohancho316@gmail.com>
2021-07-02 22:23:07 -0700

    Added New Changes

--- pack_code.py
+++ pack_code.py
@@ -1,6 +1,5 @@
 ################################# Python Imported Libraries ######################################
 
-from tkinter.constants import E
 from faker import Faker
 from faker_vehicle import VehicleProvider
 import write_student_file
@@ -12,6 +11,7 @@ from tkinter import ttk
 # Create Faker Object
 fake = Faker()
 fake.add_provider(VehicleProvider)
+
 
 # Populate Student Data
 student_data = []
@@ -31,6 +31,7 @@ for x in range(10):
   }
   student_data.append(temp)
 
+
 # Invoke File Writer Option
 write_student_file.write_student_datafile(student_data)
 
@@ -39,39 +40,35 @@ write_student_file.write_student_datafile(student_data)
 
 # Create Root Window
 root = tk.Tk()
-root.title('Student Data Reader Application')
-root.geometry('400x100')
-root.resizable(False,False)
-root.config(bg='grey')
 
-# Create Frame # 1 (Entry Frame)
+# Create Frame # 1
+# Use Pack Layout Manager
+# frame_1 = ttk.Frame(root).pack(side='left',fill='x',expand=False)
+# Use Grid Layout Manager
 id_prompt_frame = ttk.Frame(root,padding=(20,10,20,0))
 id_prompt_frame.grid()
 
-root.columnconfigure(0,weight=1)
-root.rowconfigure(1,weight=0)
-
 # Create ID Input Prompt Label Widget
-student_id_label = tk.Label(id_prompt_frame,text='Student ID: ').grid(ipadx=0,padx=(0,5),ipady=0,pady=0,row=0,column=0)
+# Use Pack Layout Manager
+# student_id_label = tk.Label(frame_1,text='Enter Student ID #: ',bg='green',fg='white').pack(ipadx=10,ipady=10,side='left',fill='y',expand=False)
+# Use Grid Layout Manager
+student_id_label = tk.Label(id_prompt_frame,text='Enter Student ID #: ',bg='grey',fg='white').grid(ipadx=10,ipady=10,row=0,column=0,padx=(0,10))
 
 # Create Entry Widget
 id_number = tk.StringVar()
-id_entry_obj = ttk.Entry(id_prompt_frame,width=15,textvariable=id_number).grid(ipadx=0,padx=0,ipady=0,pady=0,row=0,column=1)
+# id_entry_obj = ttk.Entry(frame_1,width=15,textvariable=id_number).pack(ipadx=10,ipady=10,side='left',fill='y',expand=False)
+id_entry_obj = ttk.Entry(id_prompt_frame,width=15,textvariable=id_number).grid(ipadx=10,ipady=10,row=0,column=1,padx=(0,10))
 
-# Create Frame # 2 (Button Frame)
-input_button_frame = ttk.Frame(root,padding=(20,10))
-input_button_frame.grid(ipadx=0,padx=0,ipady=0,pady=0,row=1,column=0,sticky='EW')
-
-input_button_frame.columnconfigure(0,weight=1)
-input_button_frame.columnconfigure(1,weight=1)
+input_button_frame = ttk.Frame(root,padding=(20,10,20,0))
+input_button_frame.grid()
 
 # Create ID Confirm Button Widget
 def print_id_number():
   print(f"ID # Entered: {str(id_number.get() or None)}")
-id_confirm_button = ttk.Button(input_button_frame,text='confirm',command=print_id_number).grid(ipadx=0,padx=0,ipady=0,pady=0,row=0,column=0,sticky='EW')
-
-# Create Quit Button Widget
-quit_button = ttk.Button(input_button_frame,text='quit').grid(ipadx=0,padx=0,ipady=0,pady=0,row=0,column=1,sticky='EW')
+# Use Pack Layout Manager
+#id_confirm_button = ttk.Button(frame_1,text='Confirm ID Number\nPress Here',command=print_id_number).pack(ipadx=10,ipady=10,side='left',fill='y',expand=False)
+# Use Grid Layout Manager
+id_confirm_button = ttk.Button(input_button_frame,text='confirm',command=print_id_number).grid(ipadx=10,ipady=10,row=0,column=2,padx=(0,10))
 
 # Start TKinter Event Loop & Pause Python Executible Flow
 root.mainloop()
