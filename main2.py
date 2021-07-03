98cdaf130f8db8c5bd9caa9517eced4354786ba3
John Cho <yohancho316@gmail.com>
2021-07-03 16:39:53 -0700

    Cleaned Up Widgets & Fixed Frame Properties & Grid Properties

--- main2.py
+++ main2.py
@@ -40,35 +40,23 @@ write_student_file.write_student_datafile(student_data)
 # Create Root Window
 root = tk.Tk()
 root.title('Student Data Reader Application')
-root.geometry('450x100')
+root.geometry('220x100')
 root.resizable(False,False)
-# root.config(bg='grey')
+root.config(bg='grey')
 
-# Create ID & DOB Input Frame
-input_frame =ttk.Frame(root)
-input_frame.grid(ipadx=0,ipady=0,padx=0,pady=0,row=0,column=0,sticky='w')
+# Create Frame # 1 (Entry Frame)
+id_prompt_frame = ttk.Frame(root,padding=(20,10,20,0))
+id_prompt_frame.grid()
 
 root.columnconfigure(0,weight=1)
 root.rowconfigure(1,weight=0)
 
-# ID & DOB Label Widgets
-l1 = ttk.Label(input_frame,text="Student ID: ")
-l2 = ttk.Label(input_frame,text="DOB: ")
+# Create ID Input Prompt Label Widget
+student_id_label = tk.Label(id_prompt_frame,text='Student ID: ').grid(ipadx=0,padx=(0,5),ipady=0,pady=0,row=0,column=0)
 
-l1.grid(ipadx=0,ipady=0,padx=0,pady=0,row=0,column=0,sticky='we')
-l2.grid(ipadx=0,ipady=0,padx=0,row=1,column=0,sticky='we')
-
-#  Student ID & DOB Entry Widgets
-student_id = tk.StringVar()
-student_dob = tk.StringVar()
-e1 = ttk.Entry(input_frame,width=15,textvariable=student_id)
-e2 = ttk.Entry(input_frame,width=15,textvariable=student_dob)
-
-e1.grid(ipadx=0,ipady=0,padx=0,row=0,column=1,sticky='e')
-e2.grid(ipadx=0,ipady=0,padx=0,row=1,column=1,sticky='e')
-
-e1.focus()
-e2.focus()
+# Create Entry Widget
+id_number = tk.StringVar()
+id_entry_obj = ttk.Entry(id_prompt_frame,width=15,textvariable=id_number).grid(ipadx=0,padx=0,ipady=0,pady=0,row=0,column=1)
 
 # Create Frame # 2 (Button Frame)
 input_button_frame = ttk.Frame(root,padding=(20,10))
@@ -77,23 +65,13 @@ input_button_frame.grid(ipadx=0,padx=0,ipady=0,pady=0,row=1,column=0,sticky='EW'
 input_button_frame.columnconfigure(0,weight=1)
 input_button_frame.columnconfigure(1,weight=1)
 
-# Print to Terminal Student ID & DOB Whenever Button is Activated
-def confirm_button_click():
-  print(f"Student ID Number: {student_id.get() or None}\nStudent DOB: {student_dob.get() or None}")
+# Create ID Confirm Button Widget
+def print_id_number():
+  print(f"ID # Entered: {str(id_number.get() or None)}")
+id_confirm_button = ttk.Button(input_button_frame,text='confirm',command=print_id_number).grid(ipadx=0,padx=0,ipady=0,pady=0,row=0,column=0,sticky='EW')
 
-# Confirm & Quit Button Widgets
-confirm_button = ttk.Button(input_frame,text='confirm',command=confirm_button_click)
-confirm_button.grid(ipadx=0,ipady=0,padx=0,row=2,column=0,columnspan=2,sticky='we')
-
-quit_button = ttk.Button(input_frame,text='quit',command=root.destroy)
-quit_button.grid(ipadx=0,ipady=0,padx=0,row=3,columnspan=2,sticky='we')
-
-# Create Info Frame
-info_frame = ttk.Frame(root)
-info_frame.grid(ipadx=0,ipady=0,padx=0,pady=0,row=0,column=1,sticky='e')
-
-info_label = ttk.Label(info_frame,text='Student Information Goes Here Later')
-info_label.grid(ipadx=0,ipady=0,padx=0,pady=0,row=0,column=0,sticky='ew')
+# Create Quit Button Widget
+quit_button = ttk.Button(input_button_frame,text='quit',command=root.destroy).grid(ipadx=0,padx=0,ipady=0,pady=0,row=0,column=1,sticky='EW')
 
 # Start TKinter Event Loop & Pause Python Executible Flow
 root.mainloop()
